e363b37f09600438de2043daab286636f8f981b3
chmielny <michal.chmielo@gmail.com>
2017-11-30 23:54:05 +0100

    prepdat
    -wycinanie prawdziwych donorow i akceptorow

--- PrepareData.R
+++ PrepareData.R
@@ -1,25 +1,33 @@
-atrNum = 10                                                    # ustalenie liczby atrybutow kazdego z przykladow
+atrNum = 10                                                                 # ustalenie liczby atrybutow kazdego z przykladow
 
+myCut = function(index, str) {                                                                  # funkcja wycina podciag atrNum znakow
+    tmp = substr(x = str, start = index - (atrNum / 2), stop = index + (atrNum / 2) -1)         # z duzego ciagu znakow
+    invisible(tmp)
+}
 
-cutSeq = function(str, index) {
-    tmp = substr(x = str, start = index[[1]][1] - (atrNum / 2), stop = index[[1]][1] + (atrNum / 2) -1)
+cutSeq = function(str, index) {                                                                 # funkcja wycina wiele podciagow znakow
+    tmp = sapply(index, myCut, str, simplify = TRUE, USE.NAMES = FALSE)                         # na podstawie listy indexow
     invisible(tmp)
 }
 
 prepareData = function(inputFile) {
-    raw = readLines(inputFile)                                  # wczytaj caly plik z danymi DNA
-    intronBordersLines = grep(raw, pattern="Introns") + 1       # znajdz numery lini zawierajace poczatki i konce intronów
-    intronBorders = raw[intronBordersLines]                     # znajdz liczby okreslajace poczatki i konce intronów
+    raw = readLines(inputFile)                                              # wczytaj caly plik z danymi DNA
+    intronBordersLines = grep(raw, pattern="Introns") + 1                   # znajdz numery lini zawierajace poczatki i konce intronów
+    intronBorders = raw[intronBordersLines]                                 # znajdz liczby okreslajace poczatki i konce intronów
+    
     DNAseqLines = grep(raw, pattern="Data") + 1
-    DNAseq = raw[DNAseqLines]                                   # to samo dla sekwencji DNA
+    DNAseq = raw[DNAseqLines]                                               # to samo dla sekwencji DNA
 
-    intronBorders = substring(intronBorders, 2)                 # usuniecie pierwszej spacji z ciagow 
-    intronBorders = strsplit(intronBorders, split = " ")        # stringi na listy stringow
-    intronBorders = lapply(intronBorders, as.numeric)           # listy stringow na listy numerow
+    intronBorders = substring(intronBorders, 2)                             # usuniecie pierwszej spacji z ciagow 
+    intronBorders = strsplit(intronBorders, split = " ")                    # stringi na listy stringow
+    intronBorders = lapply(intronBorders, as.numeric)                       # listy stringow na listy liczb
 
-
-    out = mapply(cutSeq, DNAseq, intronBorders, MoreArgs = NULL, SIMPLIFY = TRUE, USE.NAMES = FALSE)
-    out
+    donorIndex = lapply(intronBorders, function(l) l[c(TRUE,FALSE)])        # pozycje donorow - nieparzyste w listach
+    akceptorIndex = lapply(intronBorders, function(l) l[c(FALSE,TRUE)])     # pozycje akceptorow - parzyste
+ 
+    trueDonor = mapply(cutSeq, DNAseq, donorIndex, MoreArgs = NULL, SIMPLIFY = TRUE, USE.NAMES = FALSE)         # wyciecie z sekwencji prawdziwych donorow
+    trueAkceptor = mapply(cutSeq, DNAseq, akceptorIndex, MoreArgs = NULL, SIMPLIFY = TRUE, USE.NAMES = FALSE)   # wyciecie z sekwencji prawdziwych akceptorow
+#    out
 
 }
 

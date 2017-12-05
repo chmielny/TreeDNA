dcdb4c6b68582647a17690ef5bda89c5f2827d52
chmielny <michal.chmielo@gmail.com>
2017-12-05 01:10:21 +0100

    partdat
    -dzialajaca wersja

--- PartitionData.R
+++ PartitionData.R
@@ -1,11 +1,17 @@
 #skrypt do podzialu danych na trenujace i walidacyjne
-
+#potrzebny jest pakiet caret
 
 partitionData = function(inpCSV, outTrain, outCheck) {
     data = read.csv(inpCSV)
-    test = createDataPartition(y=data$Y,p=0.75, list=FALSE)
-    print(test)
+    trainIndex = createDataPartition(y=data$Y, p=0.7, list=FALSE)           # wykorzystanie funkcji z pakietu caret
+    trainData = within(data[trainIndex,], rm(X))                            # ktora dzieli zbior w stosunku 0.7/0.3
+    rownames(trainData) <- NULL                                             # zachowujac w przyblizeniu stosunek prawdziwych do falszywych donorow/akceptorow
+    validData = within(data[-trainIndex,], rm(X))
+    rownames(validData) <- NULL
+    write.csv(file=outTrain, x=trainData)                                   # zapis dataframe do plikow
+    write.csv(file=outCheck, x=validData)                                   # zapis dataframe do plikow
 }
 
 library(caret)
-partitionData("akceptory.csv", "a", "b")
+partitionData("donory.csv", "donory_trening.csv", "donory_test.csv")
+partitionData("akceptory.csv", "akceptory_trening.csv", "akceptory_test.csv")

32ae9debf196e34715ad6f41baef1f98ef140da7
chmielny <michal.chmielo@gmail.com>
2017-12-07 23:21:43 +0100

    cartest
    -proby z wycinaniem kolumn i rysowaniem ROC

--- CARTtest.R
+++ CARTtest.R
@@ -2,11 +2,18 @@ CARTtest = function(trainCSV, testCSV) {
     trainData = read.csv(trainCSV)
     testData =  read.csv(testCSV)
     trainData = within(trainData, rm(X))                            
-    testData = within(testData, rm(X))                            
-    tree = rpart(Y ~ ., data = trainData, method = "class")
-    pred = predict(tree, newdata = testData,type = "class") 
+    testData = within(testData, rm(X)) 
+	
+	tmpData = trainData[,c(10:60,101)]						#wycinanie kolumn z dataframe, X10-X60, 101 to Ye                     
+    tree = rpart(Y ~ ., data = tmpData, method = "class")
+    pred = predict(tree, newdata = testData,type = "class")
+#	print(predict(tree, type = "prob")[, 2]) 
+	pred = prediction(predict(tree, newdata = testData, type = "prob")[, 2], testData$Y)
     print(tree)
+	plot(performance(pred, "tpr", "fpr"))
+	abline(0, 1, lty = 2)
 }
 
 library(rpart)
+library("ROCR")
 CARTtest("donory_trening.csv", "donory_test.csv")

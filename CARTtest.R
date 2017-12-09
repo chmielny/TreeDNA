39e70dfa8f343052df8a71ad891bb53607e54bc9
Maria Chmielowiec <michal.chmielo@gmail.com>
2017-12-10 00:12:09 +0100

    cartest
    -poprawka w zapisie najlepszych A i B

--- CARTtest.R
+++ CARTtest.R
@@ -4,25 +4,22 @@ CARTtest = function(trainCSV, testCSV) {
     trainData = within(trainData, rm(X))                            
     testData = within(testData, rm(X))
     bestAUC = 0;
-    bestA = vector(mode="numeric", length=0)
-    bestB = vector(mode="numeric", length=0)
+    bestAB = data.frame(A = integer(), B = integer())
     for (A in 5:50){
         for (B in 5:49)	{
-            cutTrainData = trainData[,c( (51-A):(51+B) , 101)]						# wycinanie kolumn z dataframe - badanie param. A i B, 101 to Ye                     
+            cutTrainData = trainData[,c( (51-A):(51+B), 101)]						# wycinanie kolumn z dataframe - badanie param. A i B, 101 to Ye                     
             tree = rpart(Y ~ ., data = cutTrainData, method = "class")					# tworzenie modelu za pomoca algorytmu CART
             pred = prediction(predict(tree, newdata = testData, type = "prob")[, 2], testData$Y)	# predykcja prawdopodobienstw w lisciach dla danych testowych
             auc = performance(pred, measure = "auc")							# obliczenie auc
             auc <- auc@y.values[[1]]  
             if (auc >= bestAUC) {
                 bestAUC = auc
-                append(bestA, A) 
-                append(bestB, B) 
-	    }	
+	        bestAB[nrow(bestAB) + 1,] = c(A, B)
+            }	
         }
     }
     print(bestAUC)
-    print(bestA)
-    print(bestB)
+    print(bestAB)
 
 }
 

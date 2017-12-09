a60fa4974cf21bc7c05c6a79af7644c4619eb706
Maria Chmielowiec <michal.chmielo@gmail.com>
2017-12-09 16:41:33 +0100

    cartest
    -obliczanie auc

--- CARTtest.R
+++ CARTtest.R
@@ -4,14 +4,13 @@ CARTtest = function(trainCSV, testCSV) {
     trainData = within(trainData, rm(X))                            
     testData = within(testData, rm(X)) 
 	
-	tmpData = trainData[,c(10:60,101)]						#wycinanie kolumn z dataframe, X10-X60, 101 to Ye                     
-    tree = rpart(Y ~ ., data = tmpData, method = "class")
-    pred = predict(tree, newdata = testData,type = "class")
-#	print(predict(tree, type = "prob")[, 2]) 
-	pred = prediction(predict(tree, newdata = testData, type = "prob")[, 2], testData$Y)
+    cutTrainData = trainData[,c(10:60,101)]							# wycinanie kolumn z dataframe, X10-X60, 101 to Ye                     
+    tree = rpart(Y ~ ., data = cutTrainData, method = "class")					# tworzenie modelu za pomoca algorytmu CART
+    pred = prediction(predict(tree, newdata = testData, type = "prob")[, 2], testData$Y)	# predykcja prawdopodobienstw w lisciach dla danych testowych
     print(tree)
-	plot(performance(pred, "tpr", "fpr"))
-	abline(0, 1, lty = 2)
+    auc = performance(pred, measure = "auc")							# obliczenie auc
+    auc <- auc@y.values[[1]]  
+    print(auc)
 }
 
 library(rpart)

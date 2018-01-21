4b4d650bc61c6af683a3ead35cbf8e58ba905745
chmielny <michal.chmielo@gmail.com>
2018-01-21 02:31:54 +0100

    poprawki w CARTtest

--- CARTtest.R
+++ CARTtest.R
@@ -4,7 +4,7 @@ CARTtest = function(trainCSV, testCSV) {
     trainData = within(trainData, rm(X))                            
     testData = within(testData, rm(X))
     bestAUC = 0;
-    bestAB = data.frame(A = integer(), B = integer())
+    bestAB = data.frame(A = integer(), B = integer(), AUC = double())
     for (A in 5:50){
         for (B in 5:49)	{
             cutTrainData = trainData[,c( (51-A):(51+B), 101)]						# wycinanie kolumn z dataframe - badanie param. A i B, 101 to Ye                     
@@ -14,15 +14,18 @@ CARTtest = function(trainCSV, testCSV) {
             auc <- auc@y.values[[1]]  
             if (auc >= bestAUC) {
                 bestAUC = auc
-	        bestAB[nrow(bestAB) + 1,] = c(A, B)
+	        bestAB[nrow(bestAB) + 1,] = c(A, B, auc)
             }	
         }
     }
     print(bestAUC)
-    print(bestAB)
+    print(bestAB[bestAB$AUC == bestAUC, ])
 
 }
 
 library(rpart)
 library("ROCR")
+print("Test algorytmu CART dla donorów.")
 CARTtest("donory_trening.csv", "donory_test.csv")
+print("Test algorytmu CART dla akceptorow.")
+CARTtest("akceptory_trening.csv", "akceptory_test.csv")
